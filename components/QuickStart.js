f6053d93928a8052dd1d03599b7619d26dd78500
Abdulrhaman Alzahrani <alzahrani.abdulrahman3@gmail.com>
2022-08-16 20:31:04 +0300

    Updated HomeScreen

---
+++ components/QuickStart.js
@@ -0,0 +1,34 @@
+import {View, Text, TouchableOpacity} from 'react-native';
+import React from 'react';
+import LinearGradient from 'react-native-linear-gradient';
+
+const QuickStart = () => {
+  return (
+    <View className="items-center">
+      {/* FIXME: adjust the font and the button as the design */}
+      <Text className="text-white mt-12 font-normal text-sm text-center">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+        malesuada pellentesque pharetra libero. Cras proin posuere risus, ut.
+        Nunc nullam congue mi suspendisse rhoncus. Fermentum, bibendum tempus,
+        ullamcorper.
+      </Text>
+      <TouchableOpacity>
+        <LinearGradient
+          className="py-3 px-20 rounded-full mt-10"
+          start={{x: 1, y: 0}}
+          end={{x: 0, y: 0}}
+          colors={['#FA3B89', '#E10D60']}>
+          <Text className="text-base font-semibold text-white">
+            Quick start
+          </Text>
+        </LinearGradient>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default QuickStart;
+
+// this the primary red color https://tailwindcss.com/docs/background-image#linear-gradients
+// #E10D60
+// #FA3B89
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -11,25 +11,18 @@ import {
 import React, {useEffect, useLayoutEffect, useState} from 'react';
 
 // Components
-import TodaysQuickStartCard from '../components/TodaysQuickStartCard';
+import QuickStart from '../components/QuickStart';
 
 const HomeScreen = () => {
   return (
     <SafeAreaView className="bg-[#112044] flex-1">
       <View className="items-center p-16">
-        <Text className="mt-8 font-semibold text-4xl text-white">Welcome!</Text>
+        <Text className="mt-1 font-semibold text-4xl text-white">Welcome!</Text>
         <Image
-          className="mt-14"
+          className="mt-12"
           source={require('../asset/icn_background.png')}
         />
-        <Text
-          style={{fontFamily: 'poppins'}}
-          className="text-white mt-12 font-normal text-sm text-center">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-          malesuada pellentesque pharetra libero. Cras proin posuere risus, ut.
-          Nunc nullam congue mi suspendisse rhoncus. Fermentum, bibendum tempus,
-          ullamcorper.
-        </Text>
+        <QuickStart />
       </View>
     </SafeAreaView>
   );
